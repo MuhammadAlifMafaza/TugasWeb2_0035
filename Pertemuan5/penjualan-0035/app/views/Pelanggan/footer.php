f1a039304e41dc99ef400a051cb15f00615ad246
Muhammad Alif Mafaza <muhammadalifmafaza@gmail.com>
2024-11-06 19:44:33 +0700

    Belum selesai

--- Pertemuan5/penjualan-0035/app/views/Pelanggan/footer.php
+++ Pertemuan5/penjualan-0035/app/views/Pelanggan/footer.php
@@ -1,7 +1,7 @@
 <!-- footer.php -->
 <div class="container mt-4">
     <footer class="text-center py-3">
-        <p>&copy; 2024 Sistem Penjualan - 23.230.0035</p>
+        <p>&copy; 2024 Sistem Penjualan - </p>
     </footer>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
